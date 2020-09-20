6bbfe51f0b9ea4c96b7bb524058a610a5b9be3bc
FaziletKosure <faziletkosure1@gmail.com>
2020-09-20 22:11:56 +0200

    updated

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // random value generated 
-const y = Math.floor(Math.random() * 100 + 1); 
-console.log(y);
+const guessMe = Math.floor(Math.random() * 100 + 1); 
+console.log(Math.random());
+console.log(guessMe);
         
 // counting the number of guesses 
 // made for correct Guess 
 let guess = 0; 
 const result=document.getElementById('result');
-        
+const rabbit=document.getElementById('rabbit');
 document.getElementById("submitguess").onclick = function(){ 
     // number guessed by user	 
     if(guess==5){
@@ -14,12 +15,26 @@ document.getElementById("submitguess").onclick = function(){
         location.reload()
         return;
     }
-    const x = document.getElementById("guessField").value; 
+    const inputVal = document.getElementById("guessField").value; 
+    if(inputVal<0||isNaN(inputVal)){
+       alert("Please enter only positive numbers between 1-100"); 
+     
+    }
         
-    if(x == y) {
+    if(inputVal == guessMe) {
+        guess++;
         result.innerHTML=(`<h2 id="answer">You are the winner &#128521 You guest after
         ${guess} times!</h2>`); 
-    } else if(x > y) {	/* if guessed number is greater 
+        rabbit.style.visibility="visible";
+        rabbit.style.transitionDuration="1000ms";
+        if (window.matchMedia("(min-width: 600px)").matches) {
+            rabbit.style.transform= "scale(1.2) translate(-5%, -20%)";
+        } else {
+            rabbit.style.transform= "scale(1.0) translate(25%, 20%)";
+        }
+      
+       
+    } else if(inputVal > guessMe) {	/* if guessed number is greater 
                         than actual number*/ 
         guess++; 
         result.innerHTML=(`<h2 id="answer">I am thinking about lower number</h2>
